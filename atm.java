04fcc00b7ab248be631e819bf6478a551c20bd82
puspendra kumar paandey <90213390+pandey48@users.noreply.github.com>
2022-08-22 09:42:16 +0530

    Add files via upload

---
+++ atm.java
@@ -0,0 +1,19 @@
+package atmproject;
+import java.io.IOException;
+
+
+public class atm extends ChoiceMenu {
+
+	public static void main(String[] args) throws IOException
+	{
+		// TODO Auto-generated method stub
+		ChoiceMenu choice=new ChoiceMenu();
+		choice.getLogin();
+		
+		
+	
+
+	}
+
+
+}
